90dcd57adb4f60678821d82332c06625b7d64665
zorik19 <zorikkorolev@gmail.com>
2022-06-29 23:04:42 +0300

    decorTOR

--- tcpimage/timetable/services_git.py
+++ tcpimage/timetable/services_git.py
@@ -56,9 +56,86 @@ some_list = [1, 2, '3']
 print(list(filter(lambda x: isinstance(x, int), some_list)))
 
 
+def decorator_function(func):
+    def wrapper():
+        print('Функция-обёртка!')
+        print('Оборачиваемая функция: {}'.format(func))
+        print('Выполняем обёрнутую функцию...')
+        func()
+        print('Выходим из обёртки')
+    return wrapper
 
 
+@decorator_function
+def start_deco():
+    print('hello deco')
+
+start_deco()
 
 
+def benchmark(func):
+    import time
 
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end-start))
+    return wrapper
+
+@benchmark
+def fetch_webpage():
+    import requests
+    webpage = requests.get('https://google.com')
+
+fetch_webpage()
+
+
+def benchmark(func):
+    import time
+
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        return_value = func(*args, **kwargs)
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end-start))
+        return return_value
+    return wrapper
+
+@benchmark
+def fetch_webpage(url):
+    import requests
+    webpage = requests.get(url)
+    return webpage.text
+
+webpage = fetch_webpage('https://google.com')
+print(webpage)
+
+
+def benchmark(iters):
+    def actual_decorator(func):
+        import time
+
+        def wrapper(*args, **kwargs):
+            total = 0
+            for i in range(iters):
+                start = time.time()
+                return_value = func(*args, **kwargs)
+                end = time.time()
+                total = total + (end-start)
+            print('[*] Среднее время выполнения: {} секунд.'.format(total/iters))
+            return return_value
+
+        return wrapper
+    return actual_decorator
+
+
+@benchmark(iters=10)
+def fetch_webpage(url):
+    import requests
+    webpage = requests.get(url)
+    return webpage.text
+
+webpage = fetch_webpage('https://google.com')
+print(webpage)
 
